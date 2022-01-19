4ebd0cdc38116b6233d20ef3d4c30cb5a7b60aa5
hensetiawan <hendystyawan74@gmail.com>
2022-01-19 20:43:21 +0700

    basics event handling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,14 @@
 import "../css/task.css";
 const Task = (props) => {
+  const deleteHandler = () => {
+    console.log("delete task item");
+  };
   return (
     <div>
       <div className="card">
         <h1 className="title">{props.title}</h1>
         <p>{props.description}</p>
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   );
